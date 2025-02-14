f7ce4fb2fb2df40d86c09be08c9f8af6ff5c28ae
khadvi.pidikiti <khaaddwi.pidikiti.09@gmail.com>
2025-02-14 18:58:56 +0530

    item cards

--- src/app/e-commerce/e-commerce.component.ts
+++ src/app/e-commerce/e-commerce.component.ts
@@ -12,6 +12,63 @@ export class ECommerceComponent {
 
   userText = {user:'', pass:'' }
   message = '';
+  currentTab = 'items';
+  itemDetails:any = [
+    {
+      name: 'Kurama',
+      title: 'Nine Tailed Beast',
+      id: '9',
+      img: 'https://th.bing.com/th/id/OIP.dBSnhZQxV8dWPdSbcoOSmgHaF6?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Gyuki',
+      title: 'The Eight-Tailed Octo-Ox Beast',
+      id: '8',
+      img: 'https://vignette.wikia.nocookie.net/every-universe/images/a/ae/Hachibi_render_request_by_bechienot-d57j07e.png/revision/latest?cb=20170513065133'
+    },
+    {
+      name: 'Chomei',
+      title: 'The Seven-Tailed Flying Beetle Beast',
+      id: '7',
+      img:'https://vignette.wikia.nocookie.net/wwwdynapaul/images/5/57/Prototype_Seven_Tails.png/revision/latest?cb=20141120051850'
+    },
+    {
+      name: 'Saiken',
+      title: 'The Slimy Six-Tailed Slug Beast',
+      id: '6',
+      img: 'https://64.media.tumblr.com/a74632d81d6133d835da5f0cb693237a/394ffd127f10d1e8-02/s1280x1920/1098d395d1dfbb2be9672f83a988aa74ec9043ce.jpg'
+    },
+    {
+      name: 'Kokuo',
+      title: 'The Majestic Five-Tailed Beast',
+      id: '5',
+      img: 'https://th.bing.com/th/id/OIP.FwfslGjpqHQdki_0qTaH2wHaFk?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Son Goku',
+      title: 'The Four-Tailed Monkey King Beast',
+      id: '4',
+      img: 'https://th.bing.com/th/id/OIP.4Gn4rRFF-XRvMd_tbwGePAHaHI?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Isobu ',
+      title: 'The Three-Tailed Armored Sea Beast',
+      id: '3',
+      img: 'https://qph.cf2.quoracdn.net/main-qimg-42b705996d954f3eed0c71176c072f77'
+    },
+    { 
+      name: 'Matatabi ',
+      title: ' The Fiery Two-Tailed Cat Beast ',
+      id: '2',
+      img: 'https://qph.cf2.quoracdn.net/main-qimg-dae22dd5f3ce1dd34d2644f5d5cdfe39'
+    },
+    {
+      name: 'Shukaku ',
+      title: 'The Sandy One-Tailed Tanuki Beast',
+      id: '1',
+      img: 'https://th.bing.com/th/id/OIP.l-gIDCGzyghN9kRTE3ZaSgAAAA?rs=1&pid=ImgDetMain'
+    }
+  ]
 
   users:any = [
     {
@@ -83,6 +140,18 @@ export class ECommerceComponent {
   createAccount(){
     this.users.push(this.userText);
     // console.log(this.users);
+  }
+
+  activeTab(p:any){
+    if(p == 'items'){
+      this.currentTab = 'items';
+    }
+    else if(p == 'cart'){
+      this,this.currentTab = 'cart';
+    }
+    else if(p == 'account'){
+      this.currentTab = 'account'
+    }
   }
 
 }
